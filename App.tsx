991f078e3f281cce6e451ab42b38e4884acc8f3c
Manuel Meneses <manuelmenesesg@hotmail.com>
2022-07-07 11:51:29 -0400

    chore: implementing navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import {Home} from './src/screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home, List} from './src/screens';
+
+export type StackNavigatorParamList = {
+  Home: undefined;
+  List: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackNavigatorParamList>();
 
 const App = (): JSX.Element => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="List" component={List} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;

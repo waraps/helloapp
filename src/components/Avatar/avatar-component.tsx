40175edc28fbf0e54d9c2469c98eb0aec7b3100e
Manuel Meneses <manuelmenesesg@hotmail.com>
2022-07-25 16:17:54 -0400

    implementeing fast image + adding new props

--- src/components/Avatar/avatar-component.tsx
+++ src/components/Avatar/avatar-component.tsx
@@ -1,41 +1,49 @@
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import FastImage from 'react-native-fast-image';
 
 interface IAvatarComponent {
   uri?: string;
+  size?: number;
+  borderColor?: string;
 }
 
-export const AvatarComponent = ({uri}: IAvatarComponent) => {
+export const AvatarComponent = ({uri, size, borderColor}: IAvatarComponent) => {
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: size ? size + wp('2%') : wp('20%'),
+      height: size ? size + wp('2%') : wp('20%'),
+      borderWidth: 2,
+      borderColor: borderColor ? borderColor : '#333',
+      borderRadius: size ? size : wp('20%'),
+    },
+    tinyLogo: {
+      alignSelf: 'center',
+      width: size ? size : wp('20%'),
+      height: size ? size : wp('20%'),
+      borderRadius: size ? size / 2 : wp('10%'),
+    },
+  });
+
   const image: string = uri
     ? uri
     : 'https://cdn-icons-png.flaticon.com/512/7285/7285300.png';
 
   return (
     <View style={styles.container}>
-      <Image
+      <FastImage
         style={styles.tinyLogo}
         source={{
           uri: image,
+          priority: FastImage.priority.normal,
         }}
+        resizeMode={FastImage.resizeMode.contain}
       />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: 60,
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 100,
-    borderWidth: 2.5,
-    borderColor: '#333',
-    overflow: 'hidden',
-  },
-  tinyLogo: {
-    width: 60,
-    height: 60,
-    marginBottom: 2,
-  },
-});
